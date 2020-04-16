09d7b141dddeb6502b8fbf5a6e116b3a42b6a890
Gary Llil <garyjllil@gmail.com>
2020-04-15 23:26:10 -0400

    update login information

--- public/js/members.js
+++ public/js/members.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  $.get("/api/user_data").then(function(data) {
-    $(".member-name").text(data.email);
+  $('.tooltipped').tooltip();
+  $.get("/api/actor").then(function(data) {
+    $(".member-name").text(data.firstName);
   });
+
+  $.get("/api/booked_appt/:DogActorId").then(function(data){
+ if(data.length > 0){
+    for(let i = 0; i < data.length; i++){
+      $("#appointments").append("<li>" + data + "</li>").attr("class", "collection-item")
+    }
+  } else {
+    return $("#appointments").append("<li>You have no upcoming appointments!</li>").attr("class", "collection-item")
+  }
+  })
 });
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -11,8 +22,4 @@ document.addEventListener('DOMContentLoaded', function() {
   var instances = M.FloatingActionButton.init(elems, {
     toolbarEnabled: true
   });
-});
-
-$(document).ready(function(){
-  $('.tooltipped').tooltip();
 });
