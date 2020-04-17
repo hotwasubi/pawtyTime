a1eb4af8729efd305aa1c71982f5ca02f0ea6915
Robert Moel <robert.moel@rvmconsulting1.com>
2020-04-16 22:28:28 -0400

    Added method to return username of current logged in user

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -70,6 +70,20 @@ module.exports = function(app) {
     });
   });
 
+   // Route for getting some data about our user to be used client side
+   app.get("/api/user_data", function(req, res) {
+    if (!req.user) {
+      // The user is not logged in, send back an empty object
+      res.json({});
+    } else {
+      // Otherwise send back the user's email and id
+      // Sending back a password, even a hashed password, isn't a good idea
+      res.json({
+        email: req.user.email
+      });
+    }
+  });
+
   //Get DogActor Name and Address by id
   app.get("/api/actor/:id", (req, res)=>{
     db.DogActor.findOne({
