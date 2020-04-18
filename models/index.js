14543e6038b787bcf4febd9a2af62b04ce4a7244
Robert Moel <robert.moel@rvmconsulting1.com>
2020-04-17 22:48:21 -0400

    process env

--- models/index.js
+++ models/index.js
@@ -8,11 +8,12 @@ var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || "development";
 var config    = require(__dirname + "/../config/config.js")[env];
 var db        = {};
+var conn;
 
 if (process.env.JAWSDB_URL){
-  const conn = mysql.createConnection(process.env.JAWSDB_URL);
+  conn = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  const conn = mysql.createConnection({
+  conn = mysql.createConnection({
       host: 'localhost',
       user: 'root',
       password: process.env.dbpassword,
--- server.js
+++ server.js
@@ -23,7 +23,7 @@ require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync().then(function() {
   //put seed calls here -- select first if empty then seed
   app.listen(PORT, function() {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
